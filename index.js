d747d27aab03192916d6291ad2c4854e3d377ce7
Bry Cox <carl.cc@gmail.com>
2019-11-21 09:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(firstvar="roller-skate") {
+  return `This Saturday, I want to ${firstvar}!`;
+}
+
+function mondayWork(firstvar="go to the office") {
+  return `This Monday, I will ${firstvar}.`;
+}
+
+function wrapAdjective(name='*') {
+    return function(firstvar='special') {
+      return `You are ${name}${firstvar}${name}!`;
+    }
+  }
